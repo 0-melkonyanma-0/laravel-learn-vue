effa66986cafa2c4aefca1cf085fe2d629d2f4f1
John Doe <johndoe@example.com>
2023-11-24 11:17:09 +0300

    d

--- database/migrations/2023_11_24_072512_create_job_titles_table.php
+++ database/migrations/2023_11_24_072512_create_job_titles_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +17,7 @@ class CreateJobTitlesTable extends Migration
     {
         Schema::create('job_titles', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->unique();
             $table->timestamps();
         });
     }
