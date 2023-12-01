b8546084dcc2b5dd8e18ce7cf7541c96c4ac33fc
John Doe <johndoe@example.com>
2023-12-01 18:32:36 +0300

    d

--- resources/js/components/Drawer.vue
+++ resources/js/components/Drawer.vue
@@ -16,7 +16,7 @@
       <v-divider></v-divider>
     </v-list>
     <v-list v-for="group in groups" :key="group.groupName" shaped>
-      <v-subheader v-show="!drawer">{{ group.groupName }}</v-subheader>
+      <v-subheader v-if="group.show" v-show="!drawer">{{ group.groupName }}</v-subheader>
       <v-list-group class="pr-4">
         <template v-slot:activator>
           <v-list-item-avatar class="pr-4">
@@ -29,7 +29,8 @@
           </v-list-item-content>
         </template>
         <v-list-item
-          v-for="child in group.children" v-show="!drawer"
+          v-for="child in group.children" v-if="child.show"
+          v-show="!drawer"
           :key="child.name"
           :exact="false"
           class="px-4"
@@ -61,27 +62,33 @@ export default {
         {
           groupName: this.$t('users'),
           icon: 'mdi-account-multiple-outline',
+          show: (this.$can('index users') ||
+            this.$can('index departments') ||
+            this.$can('index job_titles') ||
+            this.$can('index roles')),
           children: [
-            {name: this.$t('users_list'), path: {name: 'users.index'}},
-            {name: this.$t('departments'), path: {name: 'users.departments'}},
-            {name: this.$t('job_titles'), path: {name: 'users.job-titles'}},
-            {name: this.$t('roles'), path: {name: 'users.roles'}}
+            {name: this.$t('users_list'), path: {name: 'users.index'}, show: this.$can('index users')},
+            {name: this.$t('departments'), path: {name: 'users.departments'}, show: this.$can('index departments')},
+            {name: this.$t('job_titles'), path: {name: 'users.job-titles'}, show: this.$can('index job_titles')},
+            {name: this.$t('roles'), path: {name: 'users.roles'}, show: this.$can('index roles')}
           ]
         },
         {
           groupName: this.$t('events'),
           icon: 'mdi-calendar-multiple',
+          show: (this.$can('index events') ||
+            this.$can('index statistics')),
           children: [
-            {name: this.$t('calendar'), path: {name: 'events.calendar'}},
-            {name: this.$t('statistics'), path: {name: 'events.statistics'}},
+            {name: this.$t('calendar'), path: {name: 'events.calendar'}, show: this.$can('index events')},
+            {name: this.$t('statistics'), path: {name: 'events.statistics'}, show: this.$can('index statistics')},
           ]
         },
         {
           groupName: this.$t('settlements'),
           icon: 'mdi-home-city-outline',
           children: [
-            {name: this.$t('regions'), path: {name: 'settlements.regions'}},
-            {name: this.$t('cities'), path: {name: 'settlements.cities'}},
+            {name: this.$t('regions'), path: {name: 'settlements.regions'}, show: this.$can('index settlements')},
+            {name: this.$t('cities'), path: {name: 'settlements.cities'}, show: this.$can('index settlements')},
           ]
         }
       ];
